61552cebfd8a2907853ddf524fd115bd58e0c804
Diego Brocanelli <diegod2@msn.com>
2020-12-30 11:27:45 -0300

    Realizado ajustes de código apontados pelo phpcs

--- src/Coins/I18nInterface.php
+++ src/Coins/I18nInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Money\Coins;
 
@@ -23,7 +23,7 @@ interface I18nInterface
      * Separetor money string
      */
     public function separatorDe(): string;
-    
+
     /**
      * Plural and singular, dollar.
      */
